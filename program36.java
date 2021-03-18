beecfb261af8e25b12a13d1168f34993df549a17
Masud <ma7332125@gmail.com>
2021-03-18 23:02:16 +0530

    Git changes checked

--- program36.java
+++ program36.java
@@ -2,6 +2,10 @@ public class program36 {
     public static void main(String[] args) {
         System.out.println("22222");
         System.out.println("22222");
+        int a = 5, b = 6, c;
+
+        System.out.println("Total " + (a + b));
+
     }
 
 }
